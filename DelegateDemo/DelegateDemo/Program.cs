4701c9a7db85213a594a91789b900ec886a09cf9
komalkashyap <komalkashyapmajri403@gmail.com>
2021-12-12 18:12:20 +0530

    [KOMAL KASHYAP] Added: Addition In Delegate

---
+++ DelegateDemo/DelegateDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateDemo
+{
+    //Delegate Defination or Declaration
+    public delegate int operation(int x, int y);
+      class Program
+    {
+        //method that is paases as a argument
+        //It has same signature as Delegates
+        static int Addition(int a, int b)
+        {
+            return a + b;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Delegate POC!!!");
+
+            //Delegate instantiation
+            operation obj = new operation(Addition);
+
+            //output
+            Console.WriteLine("Addition is={0}", obj(23, 27));
+            Console.WriteLine();
+        }
+    }
+}
